16f4f791ebe9aa330f47daa67cd805bd96ede6bb
Ron Braha <Ron.Braha@icloud.com>
2019-12-08 15:02:25 +0200

    arrange server directory for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const api = require('./routes/api')
+const api = require('./server/routes/api')
 const path = require('path')
 const app = express()
 
